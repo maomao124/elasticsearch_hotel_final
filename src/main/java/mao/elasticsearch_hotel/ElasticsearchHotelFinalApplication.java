44aace60724235f4f8763088bf228a8fe957835e
mao <1296193245@qq.com>
2022-06-04 20:39:24 +0800

    elasticsearch_hotel_final

---
+++ src/main/java/mao/elasticsearch_hotel/ElasticsearchHotelFinalApplication.java
@@ -0,0 +1,15 @@
+package mao.elasticsearch_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchHotelFinalApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ElasticsearchHotelFinalApplication.class, args);
+    }
+
+}
